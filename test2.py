8b1628c5d8bc1e8968d74cd8a3288995bf2e7977
Dominic Deckys <dominicdeckys@gmail.com>
2019-08-27 17:55:06 -0600

    modify

--- test2.py
+++ test2.py
@@ -2,33 +2,89 @@
 
 import io
 import picamera
-import socket
+import logging
+import socketserver
+from threading import Condition
+from http import server
 
+PAGE="""\
+<html>
+<head>
+<title>Dominatrix Security</title>
+</head>
+<body>
+<center><h1>Dominatrix Security Camera</h1></center>
+<center><img src="stream.mjpg" width="640" height="480"></center>
+</body>
+</html>
+"""
 
-# An output (as far as picamera is concerned), is just a filename or an object
-# which implements a write() method (and optionally the flush() and close()
-# methods)
-class MyOutput(object):
-    def __init__(self, filename, sock):
+class StreamingOutput(object):
+    def __init__(self, filename):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
         self.output_file = io.open(filename, 'wb')
-        self.output_sock = sock.makefile('wb')
 
     def write(self, buf):
         self.output_file.write(buf)
-        self.output_sock.write(buf)
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
 
-    def flush(self):
-        self.output_file.flush()
-        self.output_sock.flush()
 
-    def close(self):
-        self.output_file.close()
-        self.output_sock.close()
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
 
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
 
 # Connect a socket to a remote server on port 8000
-sock = socket.socket()
-sock.connect(('my_server', 8000))
+# sock = socket.socket()
+# sock.connect(('my_server', 8000))
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
@@ -36,10 +92,17 @@ with picamera.PiCamera() as camera:
 
     # Construct an instance of our custom output splitter with a filename
     # and a connected socket
-    my_output = MyOutput('output.h264', sock)
+    output = StreamingOutput('output.h264')
 
     # Record video to the custom output (we need to specify the format as
     # the custom output doesn't pretend to be a file with a filename)
-    camera.start_recording(my_output, format='h264')
-    camera.wait_recording(30)
-    camera.stop_recording()
+    # camera.start_recording(my_output, format='h264')
+    camera.start_recording(output, format='mjpeg')
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
+    # camera.wait_recording(30)
+    # camera.stop_recording()
